8eb9538e4382a5eb40bc8d86637948dc53bc8e87
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-01-13 13:21:52 -0500

    added Axios dependencies / successful fetch from bookmarks backend

--- src/Components/Bookmarks.js
+++ src/Components/Bookmarks.js
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Bookmark from "./Bookmark";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL;
 
 function Bookmarks() {
-  const [bookmarks] = useState([]);
+  const [bookmarks, setBookmarks] = useState([]);
+
+  useEffect(() => {
+      //http://www.localhost:8000 + /bookmarks
+      axios.get(API + "/bookmarks")
+      .then((res) => {
+        setBookmarks(res.data);
+        console.log(res.data);
+      }).catch((error) => {
+        throw error;
+      });
+  }, []);
   return (
     <div className="Bookmarks">
       <section>
